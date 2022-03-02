0c82748eeaeb76abd134ce9caeb7061c5bafecb9
Pramun05 <prasannamundhada9@gmail.com>
2022-03-02 11:46:33 +0530

    Add class Calculator

---
+++ Prj1/Calculator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prj1
+{
+    internal class Calculator
+    {
+        public int Addition(dynamic a,dynamic b)
+        {
+            return a + b;
+        }
+
+        public int Subtraction(dynamic a,dynamic b)
+        {
+            if (a < b)
+                throw new ArgumentException("a is leass than b");
+            return a - b;
+              
+        }
+    }
+}
